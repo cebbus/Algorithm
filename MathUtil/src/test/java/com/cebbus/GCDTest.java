4e8777054d6ad8590572aefd58b6f2b7035fa3a2
cebbus <cebbus@gmail.com>
2018-10-23 22:22:07 +0300

    Redundant method call removed

--- MathUtil/src/test/java/com/cebbus/GCDTest.java
+++ MathUtil/src/test/java/com/cebbus/GCDTest.java
@@ -2,15 +2,13 @@ package com.cebbus;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class GCDTest {
     @Test
     public void findGCD() throws Exception {
 
         int gcd = GCD.findGCD(162, 72);
 
-        assert Integer.compare(18, gcd) == 0;
+        assert 18 == gcd;
     }
 
 }
