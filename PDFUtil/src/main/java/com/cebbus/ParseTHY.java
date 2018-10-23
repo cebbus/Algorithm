2f630860b9c5c64b6a205e3c8ba673f00e7406e4
cebbus <cebbus@gmail.com>
2018-10-23 22:44:07 +0300

    Pdf util initialize

---
+++ PDFUtil/src/main/java/com/cebbus/ParseTHY.java
@@ -0,0 +1,7 @@
+package com.cebbus;
+
+/**
+ * Created by cebbus on 12.02.2017.
+ */
+public class ParseTHY {
+}
---
+++ PDFUtil/src/test/java/com/cebbus/ParseTHYTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by cebbus on 12.02.2017.
+ */
+public class ParseTHYTest {
+
+}
